079ca2572894e025e4e0665a53048bb3a54b9a10
Alessandro Neri <a.neri@student.tudelft.nl>
2026-02-12 23:18:27 +0100

    🐛 Prevented easter egg from triggering multiple times

--- Scripts/easter_egg.gd
+++ Scripts/easter_egg.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is PlayerController:
+		$CollisionShape2D.disabled = true
 		$Sprite2D.hide()
 		await get_tree().create_timer(0.75).timeout
 		queue_free()
